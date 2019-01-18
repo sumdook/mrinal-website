8b231441ee3a62aa37da1b07b12ff7cca555a5b8
soumikcv <soumiksur.me@gmail.com>
2019-01-18 22:26:26 +0530

    Added home in navbar and changed social icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,6 +50,9 @@ const Navbar = class extends React.Component {
           </div>
           <div id="navMenu" className="navbar-menu">
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/">
+                Home
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
